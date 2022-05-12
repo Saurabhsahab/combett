9f19444b20436e3452e309fc9bf80407b422f6ec
Saurabh <upadhyaysaurabh5960@gmail.com>
2022-05-12 16:04:43 +0530

    creating post endpoints

--- server/models/exp.js
+++ server/models/exp.js
@@ -1,2 +1,21 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+
+const expsch = new mongoose.Schema({
+  s_id: {
+    type: String,
+    required: true,
+  },
+  c_name: {
+    type: String,
+    required: [true, "Please enter valid company name"],
+  },
+  created_at: { type: Date, default: Date.now },
+
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("experience", expsch);
--- server/src/App.js
+++ server/src/App.js
@@ -1,10 +1,49 @@
 const express = require("express");
 const app = express();
-
+require("../dbcoo");
+const experience = require("../models/exp");
+const skill = require("../models/skill");
+const user = require("../models/user");
 const port = process.env.PORT || 3000;
+app.use(express.json());
 
-app.post("", (req, res) => {
-  res.send("testing");
+// user endpoints
+
+app.post("/user", async (req, res) => {
+  try {
+    const a = new user(req.body);
+    const gg = await a.save();
+
+    res.status(201).send(gg);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// skill endpoints
+
+app.post("/skill", async (req, res) => {
+  try {
+    const a = new skill(req.body);
+    const gg = await a.save();
+
+    res.status(201).send(gg);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// experience endpoints
+
+app.post("/experience", async (req, res) => {
+  try {
+    const a = new experience(req.body);
+    const gg = await a.save();
+
+    res.status(201).send(gg);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
 app.listen(port, () => {
