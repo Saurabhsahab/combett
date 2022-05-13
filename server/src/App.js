05d5cb6046aaa40f016dbf2e1aac096f944702d5
Saurabh <upadhyaysaurabh5960@gmail.com>
2022-05-13 19:37:55 +0530

    Complete the api

--- server/src/App.js
+++ server/src/App.js
@@ -20,6 +20,41 @@ app.post("/user", async (req, res) => {
   }
 });
 
+app.get("/user/:email", async (req, res) => {
+  try {
+    const a = await user.find({ email: req.params.email });
+    if (!a) {
+      res.status(404).send();
+    } else {
+      res.status(200).send(a);
+    }
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+app.patch("/user/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedData = req.body;
+    const options = { new: true };
+    const result = await user.findByIdAndUpdate(id, updatedData, options);
+    res.status(204).send(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/user/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await user.findByIdAndDelete(id);
+    res.status(200).send("Interview Experience Deleted!");
+  } catch (error) {
+    res.status(204).json({ message: error.message });
+  }
+});
+
 // skill endpoints
 
 app.post("/skill", async (req, res) => {
@@ -33,6 +68,41 @@ app.post("/skill", async (req, res) => {
   }
 });
 
+app.get("/skill/:id", async (req, res) => {
+  try {
+    const a = await skill.find({ s_id: req.params.id });
+    if (!a) {
+      res.status(404).send();
+    } else {
+      res.status(200).send(a);
+    }
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+app.patch("/skill/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedData = req.body;
+    const options = { new: true };
+    const result = await skill.findByIdAndUpdate(id, updatedData, options);
+    res.status(204).send(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/skill/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await skill.findByIdAndDelete(id);
+    res.status(200).send("Interview Experience Deleted!");
+  } catch (error) {
+    res.status(204).json({ message: error.message });
+  }
+});
+
 // experience endpoints
 
 app.post("/experience", async (req, res) => {
@@ -43,6 +113,41 @@ app.post("/experience", async (req, res) => {
     res.status(201).send(gg);
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+
+app.get("/experience/:id", async (req, res) => {
+  try {
+    const a = await experience.find({ s_id: req.params.id });
+    if (!a) {
+      res.status(404).send();
+    } else {
+      res.status(200).send(a);
+    }
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+app.patch("/experience/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedData = req.body;
+    const options = { new: true };
+    const result = await experience.findByIdAndUpdate(id, updatedData, options);
+    res.status(204).send(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/experience/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await experience.findByIdAndDelete(id);
+    res.status(200).send("Interview Experience Deleted!");
+  } catch (error) {
+    res.status(204).json({ message: error.message });
   }
 });
 
