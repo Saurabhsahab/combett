09ae10dfdec45ac7e2afb7187393cc12a6945905
Saurabh <upadhyaysaurabh5960@gmail.com>
2022-05-13 12:44:53 +0530

    Update exp.js

--- server/models/exp.js
+++ server/models/exp.js
@@ -12,6 +12,11 @@ const expsch = new mongoose.Schema({
   },
   created_at: { type: Date, default: Date.now },
 
+  title: {
+    type: String,
+    required: true,
+  },
+
   description: {
     type: String,
     required: true,
