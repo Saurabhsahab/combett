69f8e264cd4b8d391043be00b2c3ebdc4563d93a
Saurabh <upadhyaysaurabh5960@gmail.com>
2022-05-17 10:19:52 +0530

    !

--- server/src/App.js
+++ server/src/App.js
@@ -12,7 +12,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
   next();
 });
-app.post("/user", async (req,    res) => {
+app.post("/user", async (req, res) => {
   try {
     const a = new user(req.body);
     const gg = await a.save();
